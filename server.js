6337354f2df2380d62f01e63205e2f727684e8a2
Kamalie Shrie Rajendran <Kamalie.Rajendran@theinfinitechx.com>
2026-05-18 16:35:25 +0530

    fix host to 0.0.0.0 for Render

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT || 3000;
-        this.host = process.env.HOST || 'localhost';
+        this.host = process.env.HOST || '0.0.0.0';
         
         this.setupMiddleware();
         this.setupRoutes();
